6fc6dd4bd6b46f0beb5e813e26f17c3054c2da1e
ethanlhe <ethansp123@gmail.com>
2025-01-15 23:23:41 +0000

    Remove speaker icon from system tray
    The speaker icon has been removed from the SystemTray component to streamline the user interface.
    [skip gpt_engineer]

--- src/components/taskbar/SystemTray.tsx
+++ src/components/taskbar/SystemTray.tsx
@@ -39,16 +39,6 @@ export const SystemTray = ({ onCloseAllWindows }: SystemTrayProps) => {
 
   return (
     <div className="flex items-center h-full bg-gradient-to-b from-blue-600 to-blue-700 px-2 space-x-4">
-      <img 
-        src="/lovable-uploads/639a235f-bffe-4d55-838e-1944298536c6.png" 
-        alt="Volume" 
-        className="w-4 h-4 opacity-80"
-      />
-      <img 
-        src="/lovable-uploads/9933f426-7fbd-40c0-af52-90ecc7059880.png" 
-        alt="Users" 
-        className="w-4 h-4 opacity-80"
-      />
       <Dialog>
         <DialogTrigger>
           <img 
