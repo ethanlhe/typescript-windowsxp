d559b8a375876ab594ae22bafa26d57611ed6d51
ethanlhe <ethansp123@gmail.com>
2025-01-15 06:04:03 +0000

    Delete specified element from code
    [skip gpt_engineer]

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Sheet } from './ui/sheet';
 import { StartButton } from './taskbar/StartButton';
-import { QuickLaunch } from './taskbar/QuickLaunch';
 import { SystemTray } from './taskbar/SystemTray';
 import { StartMenu } from './taskbar/StartMenu';
 
@@ -15,8 +14,6 @@ export const TaskBar = ({ onCloseAllWindows }: TaskBarProps) => {
       <div className="fixed bottom-0 left-0 right-0 h-12 bg-gradient-to-r from-blue-700 to-blue-600 border-t border-blue-400 flex items-center justify-between px-1 z-50">
         <div className="flex items-center h-full gap-2">
           <StartButton />
-          <div className="h-[2px] bg-blue-400/30 mx-1" />
-          <QuickLaunch />
           <div className="h-[2px] bg-blue-400/30 mx-1" />
           <div className="flex items-center gap-1">
             {/* Window buttons will be added here in the next iteration */}
